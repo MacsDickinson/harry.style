9af75e6402ca3bdf37d220366e1782c460e593b7
Matt Burman <matt@mattburman.com>
2018-11-28 14:12:03 +0000

    DCFC (#1)
    * DCFC
    
    * Update index.js
    
    * Update index.js
    
    * lowercase filename
    
    * onMouseOver / onMouseOut instead

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,25 @@ import PropTypes from 'prop-types'
 import Layout from '../components/Layout'
 
 import pic from '../img/harry_1.jpg';
+import hoverPic from '../img/harry_dcfc.jpg';
+
+const harry = {
+	style: {
+		'-webkit-filter': 'grayscale(100%)',
+    filter: 'grayscale(100%)',
+	}
+};
 
 export default class IndexPage extends React.Component {
+	constructor() {
+		super()
+		this.state = { pic };
+	}
+
+	togglePic = () => {
+		this.setState({ pic: this.state.pic === pic ? hoverPic : pic });
+	}
+
   render() {
     const { data } = this.props
 
@@ -13,7 +30,10 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <img src={pic} alt="harry" />
+              <img
+								onMouseOut={this.togglePic} onMouseOver={this.togglePic}
+								src={this.state.pic} alt="harry" style={harry.style/* ;) */}
+							/>
             </div>
           </div>
         </section>
