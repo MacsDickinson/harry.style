4c532dcc5255bc6636a2455a18c1ead9cf501b14
Macs Dickinson <Macs.Dickinson@skybettingandgaming.com>
2018-12-20 09:46:07 +0000

    MOAR HARRIES

--- src/img/harries/index.js
+++ src/img/harries/index.js
@@ -10,6 +10,20 @@ import pic8 from './harry_8.jpg';
 import pic9 from './harry_9.jpg';
 import pic10 from './harry_10.jpg';
 import pic11 from './harry_11.jpg';
+import pic12 from './harry_12.jpg';
+import pic13 from './harry_13.jpg';
+import pic14 from './harry_14.jpg';
+import pic15 from './harry_15.jpg';
+import pic16 from './harry_16.jpg';
+import pic17 from './harry_17.jpg';
+import pic18 from './harry_18.jpg';
+import pic19 from './harry_19.jpg';
+import pic20 from './harry_20.jpg';
+import pic21 from './harry_21.jpg';
+import pic22 from './harry_22.jpg';
+import pic23 from './harry_23.jpg';
+import pic24 from './harry_24.jpg';
+import pic25 from './harry_25.jpg';
 
 export default [
     pic0,
@@ -24,4 +38,18 @@ export default [
     pic9,
     pic10,
     pic11,
+    pic12,
+    pic13,
+    pic14,
+    pic15,
+    pic16,
+    pic17,
+    pic18,
+    pic19,
+    pic20,
+    pic21,
+    pic22,
+    pic23,
+    pic24,
+    pic25
 ];
